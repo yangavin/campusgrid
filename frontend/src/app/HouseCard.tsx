db1b36c11c03b7a8939cb0be85f6df617a9c8f98
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-06-12 13:23:15 -0400

    fix problem with image and increase width of each card

--- frontend/src/app/HouseCard.tsx
+++ frontend/src/app/HouseCard.tsx
@@ -1,11 +1,10 @@
 import House from './models';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function HouseCard({image, title, price, link, baths, beds}: House){
     return (
-        <div className="w-1/6 border rounded-md p-6">
-            <Image src={image} className="w-full h-64 object-cover mb-4" alt="property"/>
+        <div className="w-1/4 border rounded-md p-6">
+            <img src={image} className="w-full h-64 object-cover mb-4" alt="property"/>
             <h2 className="mb-4">{title}</h2> 
             <p>{price}</p>
             <p>{baths} baths</p>
