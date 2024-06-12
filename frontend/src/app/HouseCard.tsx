1013faaafa6e9912795e2bbc1a70e9de23590e52
Adam.AZ <134951033+AdamAAZ@users.noreply.github.com>
2024-06-12 15:16:55 +0000

    updated

--- frontend/src/app/HouseCard.tsx
+++ frontend/src/app/HouseCard.tsx
@@ -5,7 +5,7 @@ export default function HouseCard({image, title, price, link, baths, beds}: Hous
     return (
         <div className="w-1/6 border rounded-md p-6">
             <img src={image} className="w-full h-64 object-cover mb-4" />
-            <h2 className="mb-4">{image}</h2> 
+            <h2 className="mb-4">{title}</h2> 
             <p>{price}</p>
             <p>{baths} baths</p>
             <p>{beds} beds</p>
