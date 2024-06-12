5aeab5b16cbc457c255c81cd0574f4e1e359d671
Adam.AZ <134951033+AdamAAZ@users.noreply.github.com>
2024-06-12 07:43:35 +0000

    Scraper for Kijiji and Frontenac

--- rental_scraper/scraper.py
+++ rental_scraper/scraper.py
@@ -10,7 +10,8 @@ def print_listings(listings):
 
 def scrape_kijiji():
     print("Scraping Kijiji...")
-    url = 'https://www.kijiji.ca/b-real-estate/kingston-on/c34l1700183?address=Kingston%2C%20ON&gpTopAds=y&ll=44.2334401%2C-76.49302949999999&radius=12.0&sort=dateDesc'
+    url = 'https://www.kijiji.ca/b-real-estate/kingston-on/c34l1700183?sort=dateDesc&radius=12.0&address=Kingston%2C+ON&ll=44.2334401%2C-76.49302949999999'
+    #'https://www.kijiji.ca/b-real-estate/kingston-on/c34l1700183?address=Kingston%2C%20ON&gpTopAds=y&ll=44.2334401%2C-76.49302949999999&radius=12.0&sort=dateDesc'
     response = requests.get(url) # Retreive the html content
     soup = BeautifulSoup(response.content, 'html.parser') # Parse the raw data
     
@@ -63,7 +64,14 @@ def scrape_frontenac():
         }
         listings.append(listing)
 
+def scrape_facebook_group1(): 
+    print("Scraping Facebook...")
+    url = 'https://www.facebook.com/groups/QueensUniversityStudentHousing/buy_sell_discussion'
+    response = requests.get(url)
+
 scrape_kijiji()
 scrape_frontenac()
 print("All listings:")
 print_listings(listings)
+
+#html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs xtvhhri
