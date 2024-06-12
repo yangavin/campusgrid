9bf1c9231f15f9cbb3d3e72eb9e691c10bbc94d5
Adam.AZ <134951033+AdamAAZ@users.noreply.github.com>
2024-06-12 14:58:22 +0000

    title to image test

--- frontend/src/app/HouseCard.tsx
+++ frontend/src/app/HouseCard.tsx
@@ -2,11 +2,10 @@ import House from './models';
 import Link from 'next/link';
 
 export default function HouseCard({image, title, price, link, baths, beds}: House){
-    console.log('HouseCard props:', {image, title, price, link, baths, beds}); // Check if correct
     return (
         <div className="w-1/6 border rounded-md p-6">
             <img src={image} className="w-full h-64 object-cover mb-4" />
-            <h2 className="mb-4">{title}</h2>
+            <h2 className="mb-4">{image}</h2> 
             <p>{price}</p>
             <p>{baths} baths</p>
             <p>{beds} beds</p>
