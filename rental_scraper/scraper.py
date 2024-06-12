7721c89556eb182baace6ef4895e4572ecb4dd69
Adam.AZ <134951033+AdamAAZ@users.noreply.github.com>
2024-06-12 08:09:08 +0000

    Scraper for Kijiji and Frontenac

--- rental_scraper/scraper.py
+++ rental_scraper/scraper.py
@@ -95,12 +95,3 @@ def scrape_facebook_group1():
 
 scrape_kijiji()
 scrape_frontenac()
-print("All listings:")
-print_listings(listings)
-
-#html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs xtvhhri
-=======
-
-scrape_kijiji()
-scrape_frontenac()
->>>>>>> origin/main
