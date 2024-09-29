fab0f6c5c0c612cc7de0ef8c07ebaba2187b2393
adam aaz <adam29055@gmail.com>
2024-09-29 16:44:46 -0400

    changed to port 5000

--- rental_scraper/app.py
+++ rental_scraper/app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-from scraper import listings
+from scraper import get_listings
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/listings")
-def get_listings():
-    return jsonify(listings)
+def get_listings_route():
+    return jsonify(get_listings())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3001))  # Use PORT if defined, otherwise 3001
+    port = int(os.environ.get("PORT", 5000))  # Use PORT provided by Heroku, default to 5000 for local dev
     app.run(host="0.0.0.0", port=port, debug=True)
