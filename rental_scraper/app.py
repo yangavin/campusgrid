11a00e8e8416467cfd84f4cacbfd6998dcdcb506
adam aaz <adam29055@gmail.com>
2024-09-29 16:37:20 -0400

    Update app.py to use enviornment variable for port

--- rental_scraper/app.py
+++ rental_scraper/app.py
@@ -1,14 +1,15 @@
-from scraper import listings
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from scraper import listings
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route("/listings")
 def get_listings():
     return jsonify(listings)
 
-
-app.run(port=3001, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3001))  # Use PORT if defined, otherwise 3001
+    app.run(host="0.0.0.0", port=port, debug=True)
