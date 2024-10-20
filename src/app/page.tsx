fed2cd2d87bfe9bcf792b39cc672ca3a4db03b3e
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-10-19 21:35:35 -0400

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ListingContainer from "./ListingContainer";
 export default function Home() {
   return (
     <>
-      <h1 className="text-center text-5xl my-8">StuDen</h1>
+      <h1 className="text-center text-5xl my-8">Affyto</h1>
       <ListingContainer />
     </>
   );
