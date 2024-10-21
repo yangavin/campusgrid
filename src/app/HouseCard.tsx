2d566c76bb738546ce4fb3214fe22d4618cbf557
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-10-21 01:04:41 -0400

    add theming with shadcn

--- src/app/HouseCard.tsx
+++ src/app/HouseCard.tsx
@@ -1,6 +1,14 @@
 import House from './models';
 import Link from 'next/link';
-import { Badge } from "@/components/ui/badge"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Badge } from '@/components/ui/badge';
 
 const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -10,19 +18,22 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 export default function HouseCard({image, address, price, link, baths, beds, availableDate}: House){
     return (
-        <Link href={link} target="_blank" className="w-1/5 border rounded-md p-6 animate-fade-in">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={image} className="w-full h-64 object-cover mb-4 rounded-sm" alt="property"/>
-            <h2 className="mb-4 text-xl">{address}</h2> 
-            <p className='text-2xl'>{formatter.format(price)}</p>
-
-            {/* <Badge className='text-md'>{beds} beds</Badge>
-            <br />
-            {baths && <Badge variant="secondary">{baths} baths</Badge>} */}
-
-            <p className='text-lg'>{beds} beds</p>
-            {baths && <p>{baths} baths</p>}
-            {availableDate && <p>Available: {availableDate}</p>}
+        <Link href={link} target="_blank" className="w-1/5 animate-fade-in">
+            <Card className='h-96'>
+                <CardHeader>
+                    <CardTitle>{address}</CardTitle>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img src={image} alt="house" className="w-full h-40 object-cover rounded-sm" />
+                    <p>{formatter.format(price)}</p>
+                </CardHeader>
+                <CardContent>
+                    <div className='mb-2'>
+                        <p className='text-xl'>Beds: {beds}</p>
+                        {baths && <p>Baths: {baths}</p>}
+                    </div>
+                    {availableDate && <p>Available: {availableDate}</p>}
+                </CardContent>
+            </Card>
         </Link>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import ListingContainer from "./ListingContainer";
+import { ModeToggle } from "./ThemeButton";
 
 export default function Home() {
   return (
     <>
+        <div className="flex justify-end m-4">
+          <ModeToggle/>
+        </div>
       <h1 className="text-center text-5xl my-8">Affyto</h1>
       <ListingContainer />
     </>
