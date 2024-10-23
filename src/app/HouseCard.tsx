a97794cae38cea6ff68e2e54c223c39a80145573
Gavin Yan <gavinyan10@gmail.com>
2024-10-23 01:57:03 -0400

    comprimise with frontenac image

--- src/app/HouseCard.tsx
+++ src/app/HouseCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import House from './models';
 import Link from 'next/link';
 import {
@@ -13,14 +14,17 @@ const formatter = new Intl.NumberFormat('en-US', {
     trailingZeroDisplay: 'stripIfInteger'
 });
 
-export default function HouseCard({image, address, price, link, baths, beds, availableDate}: House){
+export default function HouseCard({image, address, price, link, baths, beds, availableDate, source}: House){
     return (
         <Link href={link} target="_blank" className="xl:w-1/5 lg:w-1/4 md:1/2 w-9/12 animate-fade-in">
             <Card className='h-96'>
                 <CardHeader>
                     <CardTitle>{address}</CardTitle>
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={image} alt="house" className="w-full h-40 object-cover rounded-sm" />
+                    {source === "frontenac" ? 
+                        <img src="frontenac.png" alt="frontenac" />
+                        :
+                        <img src={image} alt="house" className="w-full h-40 object-cover rounded-sm" />
+                    }
                     <p>{formatter.format(price)}</p>
                 </CardHeader>
                 <CardContent>
