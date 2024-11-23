a588503e6c17c47e234089d159ffd6eb934e5b40
Gavin Yan <gavinyan10@gmail.com>
2024-11-23 16:54:47 -0500

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
       </body>
