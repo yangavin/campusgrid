105d6f162153f89e1f24e27b6e2c54a169982dc0
Gavin Yan <gavinyan10@gmail.com>
2024-11-29 18:53:55 -0500

    fix build error

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -15,6 +15,7 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { ViewVerticalIcon } from '@radix-ui/react-icons';
+import { Skeleton } from './skeleton';
 
 const SIDEBAR_COOKIE_NAME = 'sidebar:state';
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
