21123b8e2be2c9b7c20f7e40ceb5930600df5e8e
Gavin Yan <gavinyan10@gmail.com>
2024-12-04 22:15:58 -0500

    remove instant notifications
    that's not a thing yet

--- src/app/listings/PWAInstallPrompt.tsx
+++ src/app/listings/PWAInstallPrompt.tsx
@@ -19,7 +19,7 @@ export function PWAInstallPrompt() {
         description: (
           <div className="flex items-center gap-2">
             <img src="/icon.png" alt="Affyto" className="h-5 w-5" />
-            Install our app for a better experience and instant notifications
+            Install our app for a better experience
           </div>
         ),
         action: (
